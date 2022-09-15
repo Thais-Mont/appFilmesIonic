d6bf4791cd88d39530627ad78efcd287b8718179
Thais-Mont <thais.vmonteiro@gmail.com>
2022-09-15 14:55:56 -0300

    aplicacao em ionic

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  handlerMessage: string;
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Favoritar',
+      message: 'Deseja realmente favoritar?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.handlerMessage = 'Alert canceled';
+          },
+        },
+        {
+          text: 'SIM',
+          role: 'confirm',
+          handler: () => {
+            this.handlerMessage = 'Alert confirmed';
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 
 }
